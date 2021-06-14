37ef1684f41ea19dfe180a32a302d8e91de36aa0
syfa905 <85864763+syfa905@users.noreply.github.com>
2021-06-14 17:25:16 +0700

    Delete veruscoin2.sh

--- veruscoin2.sh
+++
@@ -1,7 +0,0 @@
-# veruscoin
-sudo apt update
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvzf nheqminer-Linux-v0.8.2.tgz
-tar xf nheqminer-Linux-v0.8.2.tar.gz
-cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RQwcGe5FpouQR8MaF4hEnnR1dsP1jaJFq6.che -p x -t 4
