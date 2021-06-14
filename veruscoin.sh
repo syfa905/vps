fbf5d0b6d1f82cf5a6306c7f1752084ddfe87e5b
syfa905 <85864763+syfa905@users.noreply.github.com>
2021-06-14 14:06:16 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,4 @@
+! wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+! tar -xvf nheqminer-Linux-v0.8.2.tgz
+! tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+! nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RQwcGe5FpouQR8MaF4hEnnR1dsP1jaJFq6.tai -p x -t 5
